9eb5ee9b8332b28010cb7594294e73cc1b2eaa71
HyeonSeong <gus8131@naver.com>
2025-03-30 15:13:09 +0900

    Chapter2 주석

--- contracts/CryptoZombie/Chapter2/zombiefactory.sol
+++ contracts/CryptoZombie/Chapter2/zombiefactory.sol
@@ -1,11 +1,15 @@
 pragma solidity >=0.5.0 <0.6.0;
 
+//함수 호출 흐름:
+//createRandomZombie → _createZombie
+//feedOnKitty → feedAndMultiply → _createZombie
+
 contract ZombieFactory {
 
     event NewZombie(uint zombieId, string name, uint dna);
 
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+    uint dnaDigits = 16; // 좀비 dna 자릿수
+    uint dnaModulus = 10 ** dnaDigits; // 16으로 제한
 
     struct Zombie {
         string name;
@@ -14,25 +18,26 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    mapping (uint => address) public zombieToOwner; // 값 -> 지갑 주소
-    mapping (address => uint) ownerZombieCount; // 지갑주소 -> 값
+    mapping (uint => address) public zombieToOwner; // 특정 좀비가 누구것인지
+    mapping (address => uint) ownerZombieCount; // 누구는 좀비를 몇개 가졌나
 
     function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
+        zombieToOwner[id] = msg.sender; // 좀비 소유권을 msg.sender 에 줌
+        ownerZombieCount[msg.sender]++; // 해당 좀비 갯수 증가
         emit NewZombie(id, _name, _dna);
     }
 
+    // view = 읽기 전용
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string memory _name) public {
-        require(ownerZombieCount[msg.sender] == 0);
+        require(ownerZombieCount[msg.sender] == 0); // 1개만 생성가능하게
         uint randDna = _generateRandomDna(_name);
-        randDna = randDna - randDna % 100;
+        randDna = randDna - randDna % 100; // 마지막 두자리 00으로 두고 쉽게 구분하게
         _createZombie(_name, randDna);
     }
 
--- contracts/CryptoZombie/Chapter2/zombiefeeding.sol
+++ contracts/CryptoZombie/Chapter2/zombiefeeding.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefactory.sol";
-contract KittyInterface {
+contract KittyInterface { //CryptoKitties의 컨트랙트에서 getKitty(uint256 _id) 함수를 호출하여 특정 Kitty의 정보를 가져올 수 있도록 정의한 인터페이스입니다
     function getKitty(uint256 _id) external view returns (
         bool isGestating,
         bool isReady,
@@ -20,12 +20,12 @@ contract ZombieFeeding is ZombieFactory {
     KittyInterface kittyContract = KittyInterface(ckAddress);
 
     function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
-        require(msg.sender == zombieToOwner[_zombieId]);
-        Zombie storage myZombie = zombies[_zombieId];
-        _targetDna = _targetDna % dnaModulus;
-        uint newDna = (myZombie.dna + _targetDna) / 2;
+        require(msg.sender == zombieToOwner[_zombieId]); // 좀비 소유자만 가능하게 함
+        Zombie storage myZombie = zombies[_zombieId]; // 해당 좀비 가져오기
+        _targetDna = _targetDna % dnaModulus; // dna 16자리 제한
+        uint newDna = (myZombie.dna + _targetDna) / 2; // 목표 + 현재 = 평균
         if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
-            newDna = newDna - newDna % 100 + 99;
+            newDna = newDna - newDna % 100 + 99; // kitty를 먹었을 경우 DNA의 마지막 두 자리를 99로 변경하여 특별한 유형의 좀비를
         }
         _createZombie("NoName", newDna);
     }
